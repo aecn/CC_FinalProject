c101799bdba9f96759877b4194acd3607beb4b69
annie <ac8502@nyu.edu>
2021-05-09 20:54:52 -0500

    push 5 - animations, critique stuff
    - eye movement works
    - got rid of static lights, working on interactive flashlight/light (from critique)
    - lit up the moon on the museum building at night (from critique)
    - cleaned up images

--- Annie_CC_Final_Project/ghosts.js
+++ Annie_CC_Final_Project/ghosts.js
@@ -4,9 +4,14 @@ class Ghost {
 
 	constructor(){
 
+		this.ghostx = width/2;
+		this.ghosty = height;
+
 	}
 
 	display(){
+
+		rect(ghostx, ghosty, 50, 50);
 
 	}
 
--- Annie_CC_Final_Project/script.js
+++ Annie_CC_Final_Project/script.js
@@ -1,9 +1,8 @@
 // Annie Chen - Final Project
 
 
-// eye inspo: https://p5js.org/examples/math-arctangent.html
 var scene = 1; // scenes start with 1, the title screen
-var day = false; // the daytime is default until KeyPressed
+let day = true; // the daytime is default until KeyPressed
 var sprite;
 // museum
 let daymuseum;
@@ -14,14 +13,23 @@ var stars = []; // array storing stars
 // pearl
 let pearl;
 let nightpearl;
+let leye;
+let reye;
+let lx = 160;
+let rx = 240;
+let ly = 248;
+let ry = 248;
 // persistence of time
 let memory;
-var warpmemory;
+var warpmemory; // animation
 // the scream
 let scream;
+let nightscream;
 // painting frames
 let vertical;
 let horizontal;
+let nightvertical;
+let nighthorizontal;
 // terracotta
 let terracotta;
 // ancient egyptian
@@ -29,7 +37,7 @@ let casket;
 let mummy;
 // roman statue
 let dayroman;
-var nightroman;
+var nightroman; // animation
 // dinosaurs
 let dino;
 let closeupdino;
@@ -45,6 +53,8 @@ function preload() {
     scream = loadImage('art/scream.jpeg');
     vertical = loadImage('art/vertical.png');
     horizontal = loadImage('art/horizontal.png');
+    nightvertical = loadImage('art/nvertical.png');
+    nighthorizontal = loadImage('art/nhorizontal.png');
     terracotta = loadImage('art/terracotta.png');
     casket = loadImage('art/casket.png');
     dayroman = loadImage('art/roman1.png');
@@ -52,6 +62,7 @@ function preload() {
     closeupdino = loadImage('art/closeupdino.png');
     // animations
     warpmemory = loadAnimation('art/memory/memory1.png', 'art/memory/memory2.png', 'art/memory/memory3.png', 'art/memory/memory4.png', 'art/memory/memory5.png', 'art/memory/memory6.png', 'art/memory/memory7.png');
+    nightscream = loadAnimation('art/scream/scream1.png', 'art/scream/scream2.png', 'art/scream/scream3.png', 'art/scream/scream4.png', 'art/scream/scream5.png', 'art/scream/scream6.png', 'art/scream/scream7.png', 'art/scream/scream8.png', 'art/scream/scream9.png');
     nightroman = loadAnimation('art/roman/roman.png', 'art/roman/roman2.png', 'art/roman/roman3.png');
     mummy = loadAnimation('art/mummy/open1.png', 'art/mummy/open2.png', 'art/mummy/open3.png', 'art/mummy/open4.png', 'art/mummy/open5.png', 'art/mummy/open6.png', 'art/mummy/open7.png');
 }
@@ -63,9 +74,14 @@ function setup() {
         stars[i] = new Star(); // star class called in array
     }
 
+    // animations 
     mummy.frameDelay = 10; // slow down speed of animation
     sprite = createSprite(650, 300);
     sprite.addAnimation("open", mummy);
+
+    // time
+
+    startTime = millis(); // start time
 }
 
 function draw() {
@@ -106,7 +122,7 @@ function draw() {
         if (day) {
             noStroke();
             // wall
-            background("#ba181b"); // red
+            background("#bfd8bd"); // teal
             // floor
             fill("#583101"); // brown
             rectMode(CENTER);
@@ -117,25 +133,23 @@ function draw() {
             fill("#000000");
             rect(190, 260, 270, 360);
             imageMode(CENTER);
-            image(pearl, 190, 270, 200, 286);
-            // Painting 1: Frame
-            image(vertical, 190, 260, 400, 533);
+            image(pearl, 190, 270, 200, 286); // painting
+            image(vertical, 190, 260, 400, 533); // frame
 
             // Painting 2: The Persistence of Memory
-            image(memory, 600, 250, 300, 228);
-            // Painting 2: Frame
-            image(horizontal, 600, 250);
+            image(memory, 600, 250, 300, 228); // painting
+            image(horizontal, 600, 250); // frame
 
             // Painting 3: The Scream
-            image(scream, 1020, 270, 300, 378);
-            // Painting 3: Frame
-            image(vertical, 1020, 270, 450, 600);
+            image(scream, 1020, 270, 300, 378); // painting
+            image(vertical, 1020, 270, 450, 600); // frame
 
             daysign(1250, 500); // sign
 
         } else {
+
             noStroke();
-            background("#660708"); // dark red wall
+            background("#384a37"); // dark 
             fill("#40250b"); // dark brown
             rectMode(CENTER);
             rect(625, 600, 1400, 100); // floor
@@ -147,23 +161,23 @@ function draw() {
             rect(190, 260, 270, 360);
             imageMode(CENTER);
             image(nightpearl, 190, 270, 200, 286); // night ver
-
-
-            image(vertical, 190, 260, 400, 533); // frame
-            fill(255, 237, 71, 50);
-            triangle(190, 0, 50, 600, 300, 600); // light
+            image(nightvertical, 190, 260, 400, 533); // frame
+            // her pupils moving, following mouse
+            fill(0); // pupil color = black
+            ellipse(lx, ly, 6); // left pupil location depends on variables
+            ellipse(rx, ry, 6); // right pupil location depends on variables
+            ly = map(mouseY, 0, height, 244, 245); // left pupil- vertical distance (y) : mouseY has range from 0 - height, map it between 244-245
+            ry = map(mouseY, 0, height, 248, 250); // right pupil- vertical distance (y) : mouseY has range from 0 - height, map it between 248-250
+            lx = map(mouseX, 0, width, 115, 122); // left pupil - horizontal distance (x) : mouseX has range from 0 - width, map it between 115-122
+            rx = map(mouseX, 0, width, 143, 155); // right pupil - horizontal distance (x) : mouseX has range from 0 - width, map it between 143-155
 
             // Painting 2: The Persistence of Memory
             animation(warpmemory, 600, 250);
-            image(horizontal, 600, 250); // frame
-            fill(255, 237, 71, 50);
-            triangle(600, 0, 450, 600, 750, 600); // light
+            image(nighthorizontal, 600, 250); // frame
 
             // Painting 3: The Scream
-            image(scream, 1020, 270, 300, 378);
-            image(vertical, 1020, 270, 450, 600);
-            fill(255, 237, 71, 50);
-            triangle(1020, 0, 850, 600, 1200, 600); // light
+            animation(nightscream, 1020, 270);
+            image(nightvertical, 1020, 270, 450, 600); // frame
 
             nightsign(1250, 500); // sign
         }
@@ -172,21 +186,19 @@ function draw() {
     if (scene == 3) { // statues/mummies display
         if (day) {
             noStroke();
-
             // wall
-            background("#245501");
+            background("#d5c6e0");
+            stand(); // stands
 
             // Terracotta Warrior
-            fill(255);
             imageMode(CENTER);
-            image(terracotta, 200, 300, 170, 498);
+            image(terracotta, 200, 280, 170, 498);
 
             // Ancient Egyptian Casket
-            image(casket, 650, 300, 400, 533);
+            image(casket, 600, 280, 400, 533);
 
             // Roman statue - Augustus of Prima Porta
             image(dayroman, 1000, 300, 400, 533);
-
 
             // floor
             fill("#583101");
@@ -198,29 +210,28 @@ function draw() {
         } else {
             noStroke();
             // wall
-            background("#143601");
+            background("#52455c");
+            nstand(); // stands
+
             // Terracotta Warrior
             fill(255);
             imageMode(CENTER);
-            image(terracotta, 200, 300, 170, 498);
+            image(terracotta, 200, 280, 170, 498);
             fill(255, 237, 71, 50);
-            triangle(190, 0, 50, 600, 300, 600); // light
-
-            // Ancient Egyptian Casket
-            drawSprites();
-            fill(255, 237, 71, 50);
-            triangle(600, 0, 450, 600, 750, 600); // light
-
 
             // Roman statue - Augustus of Prima Porta
             animation(nightroman, 1000, 300);
             fill(255, 237, 71, 50);
-            triangle(1020, 0, 850, 600, 1200, 600); // light
 
             // floor
             fill("#583101");
             rectMode(CENTER);
             rect(625, 600, 1400, 100);
+
+            // Ancient Egyptian Casket - goes to front of screen, this is why its in front of floor
+            drawSprites(); // animation
+            fill(255, 237, 71, 50);
+
 
             nightsign(1250, 500); // sign
         }
@@ -230,21 +241,37 @@ function draw() {
         if (day) {
             noStroke();
             // wall
-            background("#0496ff");
+            background("#cfebdf");
             // trex
             imageMode(CENTER);
             image(dino, 650, 380, 800, 696);
 
+            fill("#bc8a5f");
+            rect(1200, 550, 100, 100); // sign stand
+
             daysign(1250, 500); // sign
         } else {
+
+
             noStroke();
             // wall
-            background("#0a2472");
+            background("#36453e");
+
+
+            setTimeout(confusion1, 2000);
+
             // trex
             fill(0);
-            rect(500, 200, 700, 100);
             imageMode(CENTER);
-            image(closeupdino, 650, 380);
+            image(closeupdino, 600, 380);
+
+
+            fill(0);
+            ellipseMode(CENTER);
+            ellipse(1000, 600, 10, 10);
+
+            fill("#000000");
+            rect(1200, 550, 100, 100); // sign stand
             nightsign(1250, 500); // sign
 
         }
@@ -294,6 +321,55 @@ function bench(posX, posY) {
     rect(410, 600, 20, 100); // left leg shadow
     rect(790, 600, 20, 100); // right leg shadow
     rect(600, 600, 400, 20); // seat shadow
+}
+
+function stand() {
+    // terracotta
+    rectMode(CENTER);
+    fill("#ddb892");
+    rect(200, 550, 250, 80); // stand shading
+    fill("#fefae0");
+    rect(200, 540, 250, 30); // stand	
+    //mummy
+    rectMode(CENTER);
+    fill("#ddb892");
+    rect(600, 550, 250, 80); // stand shading
+    fill("#fefae0");
+    rect(600, 540, 250, 30); // stand
+    // roman 
+    rectMode(CENTER);
+    fill("#ddb892");
+    rect(1000, 550, 250, 80); // stand shading
+    fill("#fefae0");
+    rect(1000, 540, 250, 30); // stand
+}
+
+function nstand() {
+    // terracotta
+    rectMode(CENTER);
+    fill("#261a0c");
+    rect(200, 550, 250, 80); // stand shading
+    fill("#352713");
+    rect(200, 540, 250, 30); // stand	
+    //mummy
+    rectMode(CENTER);
+    fill("#261a0c");
+    rect(600, 550, 250, 80); // stand shading
+    fill("#352713");
+    rect(600, 540, 250, 30); // stand
+    // roman 
+    rectMode(CENTER);
+    fill("#261a0c");
+    rect(1000, 550, 250, 80); // stand shading
+    fill("#352713");
+    rect(1000, 540, 250, 30); // stand	
+}
+
+function confusion1() {
+    fill(255);
+    textSize(50);
+    text("?", 550, 300);
+
 }
 
 
